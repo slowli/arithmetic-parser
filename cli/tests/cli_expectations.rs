20d8a6ece5c3803a64a32b4eed5d871fc201cf91
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-10-03 14:38:05 +0300

    Replace `indoc` with `unindent`

--- cli/tests/cli_expectations.rs
+++ cli/tests/cli_expectations.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::*;
-use indoc::indoc;
 use predicates::prelude::*;
+use unindent::unindent;
 
 use std::process::Command;
 
@@ -38,51 +38,51 @@ fn outputting_interpreted_function() {
 
 #[test]
 fn syntax_error() {
-    const EXPECTED_ERR: &str = indoc! {r#"
+    const EXPECTED_ERR: &str = r#"
         error[PARSE]: Uninterpreted characters after parsing
           ┌─ Snip #1:1:5
           │
         1 │ let x = 5
           │     ^^^^^ Error occurred here
-    "#};
+    "#;
 
     let assert = create_command("let x = 5").assert();
     assert
         .failure()
         .code(1)
-        .stderr(predicate::str::starts_with(EXPECTED_ERR));
+        .stderr(predicate::str::starts_with(unindent(EXPECTED_ERR)));
 }
 
 #[test]
 fn undefined_variable_error() {
-    const EXPECTED_ERR: &str = indoc! {r#"
+    const EXPECTED_ERR: &str = r#"
         error[EVAL]: Variable `x` is not defined
           ┌─ Snip #1:1:9
           │
         1 │ 1 + 2 * x
           │         ^ Undefined variable occurrence
-    "#};
+    "#;
 
     let assert = create_command("1 + 2 * x").assert();
     assert
         .failure()
         .code(1)
-        .stderr(predicate::str::starts_with(EXPECTED_ERR));
+        .stderr(predicate::str::starts_with(unindent(EXPECTED_ERR)));
 }
 
 #[test]
 fn incompatible_arg_count_error() {
-    const EXPECTED_ERR: &str = indoc! {r#"
+    const EXPECTED_ERR: &str = r#"
         error[EVAL]: Mismatch between the number of arguments in the function definition and its call
           ┌─ Snip #1:1:1
           │
         1 │ if(2 > 1, 3)
           │ ^^^^^^^^^^^^ Called with 2 arg(s) here
-    "#};
+    "#;
 
     let assert = create_command("if(2 > 1, 3)").assert();
     assert.failure().code(1).stderr(
-        predicate::str::starts_with(EXPECTED_ERR)
+        predicate::str::starts_with(unindent(EXPECTED_ERR))
             .and(predicate::str::contains("definition requires 3 arg(s)")),
     );
 }
@@ -93,7 +93,7 @@ fn error_with_call_trace() {
         is_positive = |x| x > 0;
         is_positive(3) && !is_positive((1, 2))
     "#;
-    const EXPECTED_ERR: &str = indoc! {r#"
+    const EXPECTED_ERR: &str = r#"
         error[EVAL]: Compare requires 2 number arguments
           ┌─ Snip #1:2:27
           │
@@ -104,13 +104,13 @@ fn error_with_call_trace() {
           │                           Invalid argument
         3 │         is_positive(3) && !is_positive((1, 2))
           │                            ------------------- Call at depth 1
-    "#};
+    "#;
 
     let assert = create_command(PROGRAM).assert();
     assert
         .failure()
         .code(1)
-        .stderr(predicate::str::starts_with(EXPECTED_ERR));
+        .stderr(predicate::str::starts_with(unindent(EXPECTED_ERR)));
 }
 
 #[test]
@@ -119,7 +119,7 @@ fn error_with_call_complex_call_trace() {
         all = |array, predicate| array.fold(true, |acc, x| acc && predicate(x));
         (1, 2, map).all(|x| 0 < x)
     "#;
-    const EXPECTED_ERR: &str = indoc! {r#"
+    const EXPECTED_ERR: &str = r#"
         error[EVAL]: Compare requires 2 number arguments
           ┌─ Snip #1:2:34
           │
@@ -134,11 +134,11 @@ fn error_with_call_complex_call_trace() {
           │         │                   │   Invalid argument
           │         │                   Failed call
           │         Call at depth 3
-    "#};
+    "#;
 
     let assert = create_command(PROGRAM).assert();
     assert
         .failure()
         .code(1)
-        .stderr(predicate::str::starts_with(EXPECTED_ERR));
+        .stderr(predicate::str::starts_with(unindent(EXPECTED_ERR)));
 }

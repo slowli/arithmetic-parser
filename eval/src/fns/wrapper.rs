9302d13c6f54a29115638d08e0594ed51c9bc837
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-11-22 21:20:03 +0200

    Fix formatting

--- eval/src/fns/wrapper.rs
+++ eval/src/fns/wrapper.rs
@@ -720,7 +720,10 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{alloc::{format, ToOwned}, Environment, ExecutableModule, Prelude, WildcardId};
+    use crate::{
+        alloc::{format, ToOwned},
+        Environment, ExecutableModule, Prelude, WildcardId,
+    };
 
     use arithmetic_parser::grammars::{F32Grammar, Parse, Untyped};
     use assert_matches::assert_matches;

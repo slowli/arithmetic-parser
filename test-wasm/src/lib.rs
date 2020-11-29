082e92fa750d91439757ac2defd3fd64441a05ee
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-11-29 20:26:05 +0200

    Fix WASM example

--- test-wasm/src/lib.rs
+++ test-wasm/src/lib.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use alloc::string::ToString;
 use core::f64;
 
-use arithmetic_eval::{fns, Environment, Prelude, Value, VariableMap, WildcardId};
+use arithmetic_eval::{fns, Assertions, Environment, Prelude, Value, VariableMap, WildcardId};
 use arithmetic_parser::grammars::{F64Grammar, Parse, Untyped};
 use wasm_bindgen::prelude::*;
 
@@ -70,7 +70,7 @@ pub fn evaluate(program: &str) -> Result<JsValue, JsValue> {
     let block = Untyped::<F64Grammar>::parse_statements(program)
         .map_err(|err| Error::new(&err.to_string()))?;
 
-    let mut env = Prelude.iter().collect();
+    let mut env = Prelude.iter().chain(Assertions.iter()).collect();
     initialize_env(&mut env);
 
     let value = env

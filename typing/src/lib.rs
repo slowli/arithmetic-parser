e4f41223c0f745c2049e77eb54ef086c1c909fbd
Alex Ostrovski <ostrovski.alex@gmail.com>
2021-03-20 15:53:35 +0200

    Test version sync for typing crate

--- typing/src/lib.rs
+++ typing/src/lib.rs
@@ -53,6 +53,7 @@
 //! [`Grammar`]: arithmetic_parser::grammars::Grammar
 //! [`arithmetic-eval`]: https://crates.io/crates/arithmetic-eval
 
+#![doc(html_root_url = "https://docs.rs/arithmetic-typing/0.2.0")]
 #![warn(missing_docs, missing_debug_implementations)]
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(

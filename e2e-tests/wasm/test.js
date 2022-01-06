d1c6e22cc50824ac6054c2ddee5e06da19a99952
Alex Ostrovski <ostrovski.alex@gmail.com>
2022-01-06 11:40:16 +0200

    Fix WASM example

--- e2e-tests/wasm/test.js
+++ e2e-tests/wasm/test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { strict: assert } = require('assert');
-const { evaluate } = require('./pkg')
+const { evaluate } = require('./pkg');
 
 // Normal cases.
 const evaluated = evaluate(`
@@ -10,19 +10,19 @@ const evaluated = evaluate(`
   order = |x, y| (min(x, y), max(x, y));
   assert_eq(order(0.5, -1), (-1, 0.5));
   (_, M) = order(3^2, { x = 3; x + 5 });
-  M`)
-assert.equal(evaluated, 9)
+  M`);
+assert.equal(evaluated, 9);
 
 const evaluatedFlag = evaluate(`
   max_value = |...xs| {
-      fold(xs, -Inf, |acc, x| if(x > acc, x, acc))
+      xs.fold(-Inf, |acc, x| if(x > acc, x, acc))
   };
   max_value(1, -2, 7, 2, 5) == 7 && max_value(3, -5, 9) == 9
-`)
-assert(evaluatedFlag)
+`);
+assert(evaluatedFlag);
 
 // Parse errors.
-assert.throws(() => evaluate('1 +'), /1:4: Unfinished arithmetic expression/)
+assert.throws(() => evaluate('1 +'), /1:4: Unfinished arithmetic expression/);
 
 // Evaluation errors.
-assert.throws(() => evaluate('2 + test(1, 2)'), /1:5: Variable `test` is not defined/)
+assert.throws(() => evaluate('2 + test(1, 2)'), /1:5: Variable `test` is not defined/);

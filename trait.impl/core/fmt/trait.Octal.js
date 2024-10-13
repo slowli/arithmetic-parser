04c258c5011ec383cd81a47a18b208c5531d27cc
slowli <slowli@users.noreply.github.com>
2024-10-13 13:09:58 +0000

    Deploying to gh-pages from @ slowli/arithmetic-parser@3a5fc3251334f2721229fa5968c279cace2bf090 🚀

---
+++ trait.impl/core/fmt/trait.Octal.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"arithmetic_parser":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Octal.html\" title=\"trait core::fmt::Octal\">Octal</a> for <a class=\"struct\" href=\"arithmetic_parser/grammars/struct.Features.html\" title=\"struct arithmetic_parser::grammars::Features\">Features</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
